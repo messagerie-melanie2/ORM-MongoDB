6e85bee88fb8c9ebe31dd3264988e0dd0a60ab82
Thomas Payen <thomas.payen@i-carre.net>
2015-10-30 15:10:45 +0100

    Add tools class for PHP non existing methods (startsWith, endsWith, ...)

---
+++ tools/tools.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Ce fichier est développé pour la gestion de la librairie ORM
+ * Cette Librairie permet d'accèder aux données sans avoir à implémenter de couche SQL
+ * Des objets génériques vont permettre d'accèder et de mettre à jour les données
+ *
+ * ORM Copyright © 2015  PNE Annuaire et Messagerie/MEDDE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace ORM\Tools;
+
+/**
+ * Outils pour le traitement ORM
+ */
+class Tools {
+  /**
+   * Statswith
+   * @param string $haystack The string to search in
+   * @param string $needle The searched value
+   * @return boolean
+   */
+  public static function startsWith($haystack, $needle) {
+    // search backwards starting from haystack length characters from the end
+    return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
+  }
+  /**
+   * Endswith
+   * @param string $haystack The string to search in
+   * @param string $needle The searched value
+   * @return boolean
+   */
+  public static function endsWith($haystack, $needle) {
+    // search forward starting from end minus needle length characters
+    return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
+  }
+}
