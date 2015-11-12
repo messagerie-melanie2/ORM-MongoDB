7066572af50dc96d0a7c2c89fa2260b08f4f239c
Thomas Payen <thomas.payen@i-carre.net>
2015-11-12 11:31:08 +0100

    Update index for mysql

--- config/ida_mysql/SQL/create_index.sql
+++ config/ida_mysql/SQL/create_index.sql
@@ -1,7 +1,13 @@
+SHOW INDEXES FROM events;
+DROP INDEX events_start_end ON events;
+DROP INDEX events_recurrence_freq_until ON events;
+
 CREATE INDEX events_start_end
   USING BTREE
-  ON events (dtstart ASC, dtend DESC);
+  ON events (dtstart DESC, dtend);
 
 CREATE INDEX events_recurrence_freq_until
   USING BTREE
-  ON events (recurrence_freq, recurrence_until);
+  ON events (recurrence_freq, recurrence_until DESC);
+
+
