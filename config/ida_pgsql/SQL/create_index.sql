6b7eab5903f8d76f02fe0ca26feb51a082d0e964
Thomas Payen <thomas.payen@i-carre.net>
2015-11-12 18:57:06 +0100

    New indexes for queries

--- config/ida_pgsql/SQL/create_index.sql
+++ config/ida_pgsql/SQL/create_index.sql
@@ -11,3 +11,18 @@ CREATE INDEX events_recurrence_freq_until
   ON events
   USING BTREE
  (recurrence_freq, recurrence_until DESC);
+
+CREATE INDEX events_calendar_start
+  ON events
+  USING BTREE 
+  (calendar, dtstart);
+
+CREATE INDEX events_calendar_end
+  ON events
+  USING BTREE 
+  (calendar, dtend);
+
+CREATE INDEX events_calendar_recurrence_freq_until_end
+  ON events
+  USING BTREE 
+  (calendar, recurrence_freq, recurrence_until, dtend);
