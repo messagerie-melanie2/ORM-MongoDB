f576c9808f2774a74130db178f95cd060c1120cf
Thomas Payen <thomas.payen@i-carre.net>
2015-11-12 11:31:20 +0100

    Update index and table for pgsql

--- config/ida_pgsql/SQL/create_index.sql
+++ config/ida_pgsql/SQL/create_index.sql
@@ -1,9 +1,13 @@
+DROP INDEX events_recurrence_freq_until;
+DROP INDEX events_start_end;
+
+
 CREATE INDEX events_start_end
   ON events
   USING BTREE 
-  (dtstart ASC, dtend DESC);
+  (dtstart DESC, dtend);
 
 CREATE INDEX events_recurrence_freq_until  
   ON events
   USING BTREE
- (recurrence_freq, recurrence_until);
+ (recurrence_freq, recurrence_until DESC);
