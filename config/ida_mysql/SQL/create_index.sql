3c000d052f761a74dac533077012cc4c71539b2d
Thomas Payen <thomas.payen@i-carre.net>
2015-11-12 18:57:16 +0100

    New indexes for queries

--- config/ida_mysql/SQL/create_index.sql
+++ config/ida_mysql/SQL/create_index.sql
@@ -10,4 +10,15 @@ CREATE INDEX events_recurrence_freq_until
   USING BTREE
   ON events (recurrence_freq, recurrence_until DESC);
 
+CREATE INDEX events_calendar_start
+  USING BTREE
+  ON events (calendar, dtstart);
+
+CREATE INDEX events_calendar_end
+  USING BTREE
+  ON events (calendar, dtend);
+
+CREATE INDEX events_calendar_recurrence_freq_until_end
+  USING BTREE
+  ON events (calendar, recurrence_freq, recurrence_until, dtend);
 
